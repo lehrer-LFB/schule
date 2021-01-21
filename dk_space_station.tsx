64e9844d313321cadf3e913741385691a79c7aa1
lehrer-LFB <77414546+lehrer-LFB@users.noreply.github.com>
2021-01-21 10:13:55 +0100

    Baubeginn

---
+++ dk_space_station.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dk_space_station" tilewidth="32" tileheight="32" tilecount="80" columns="16">
+ <image source="tiles/world-tiles-master/inventory/dk_space_station.png" width="512" height="160"/>
+</tileset>
---
+++ office-tilemap_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="office-tilemap_0" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="tiles/office-tilemap_0.png" width="160" height="160"/>
+</tileset>
